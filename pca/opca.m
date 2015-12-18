5727e5023002a1ac82697cd672fe389466b8034a
Yingjie <1275963@gmail.com>
2015-12-18 20:37:17 +0800

    Draw most important PCA eigenfaces

--- pca/opca.m
+++ pca/opca.m
@@ -1,13 +1,16 @@
 clear
+imgsize = [112,92]; 
 data = imagedata();
 
 avg = mean(data,2);
 A = data - avg*ones(1, 400);
-
 L = A'*A;
 [V,D] = eig(L);
 
-principal = V(:,end);
-principal = A*principal;
+pvector = zeros(imgsize(1)*imgsize(2),30);
+for i = 0:29
+    ptemp = V(:,end-i);
+    pvector(:,i+1) = A*ptemp;
+end
 
 
--- peekbase.m
+++ peekbase.m
@@ -1,10 +1,11 @@
-function peekbase(w,imgsize,filename)
+function peekbase(w,filename)
+imgsize = [112,92]; 
 f = figure;
-for i = 1:40
+for i = 1:30
     figure(f);
     test = w(:,i);
     test = reshape(test,imgsize);
-    subplot(5,8,i);imshow((test),[])
+    subplot(5,6,i);imshow((test),[])
 end
 
 % saveas(gcf, filename, 'bmp')
