6845e29fbcd7a8ee3d94886c6855443fd54b7e8f
Yingjie <1275963@gmail.com>
2015-12-19 12:10:13 +0800

    Eigenfaces with eigenvalues as title

--- pca/peekbase.m
+++ pca/peekbase.m
@@ -1,8 +1,11 @@
+% run ocpa.m first
 imgsize = [112,92]; 
 f = figure;
 for i = 0:29
     figure(f);
+    evalues = eig(L);
     test = pvector(:,end-i);
     test = reshape(test,imgsize);
     subplot(5,6,i+1);imshow((test),[])
+    title(num2str(evalues(end-i)));
 end
