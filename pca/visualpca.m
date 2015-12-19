2e59832db9671cfdcb9f7bb3fb1b664d520f53f8
Yingjie <1275963@gmail.com>
2015-12-19 10:50:40 +0800

    Working on reconstruction

--- pca/visualpca.m
+++ pca/visualpca.m
@@ -1,6 +1,17 @@
-%% Draw eigenvalues
-% DESCRIPTIVE TEXT
-    test = eig(L);
-    plot(test);
-%% 
+figure()
+test = eig(L);
+plot(test);
 
+figure()
+imgsize = [112,92]; 
+pick = data(:,1);
+
+subplot(1,2,1);
+ori = reshape(pick,imgsize);
+imshow(ori,[]);
+
+subplot(1,2,2);
+w= pvector'*pick;
+reconstr = pvector*w;
+reconstr = reshape(reconstr,imgsize);
+imshow(reconstr,[]);
