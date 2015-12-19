502ce5153c015eb558bd6e6937506b87fd9ab8ce
Yingjie <1275963@gmail.com>
2015-12-19 11:52:46 +0800

    Got contricution rate, working on perfect reconstruction

--- pca/opca.m
+++ pca/opca.m
@@ -4,15 +4,11 @@ imgsize = [112,92];
 data = imagedata();
 
 avg = mean(data,2);
-A = data - avg*ones(1, 400);
+A = data - avg*ones(1, 400); % Centered at zero
 L = A'*A;
 [V,D] = eig(L);
 
-pvector = zeros(imgsize(1)*imgsize(2),30);
-for i = 0:29
-    ptemp = V(:,end-i);
-    pvector(:,i+1) = A*ptemp;
-    pvalue(i+1) = D(end-i,end-i);
-end
-
-
+eigenvalues = eig(L);
+sum(eigenvalues(end-99:end))/sum(eigenvalues)
+vector = V(:,end-99:end);
+pvector = A*vector;
--- pca/visualpca.m
+++ pca/visualpca.m
@@ -1,17 +1,20 @@
+% figure()
+% test = eig(L);
+% plot(test);
+clear
+opca()
 figure()
-test = eig(L);
-plot(test);
 
-figure()
 imgsize = [112,92]; 
-pick = data(:,1);
+pick = data(:,15);
 
 subplot(1,2,1);
 ori = reshape(pick,imgsize);
 imshow(ori,[]);
 
 subplot(1,2,2);
-w= pvector'*pick;
-reconstr = pvector*w;
+w = pvector'*(pick-avg);
+w = w./norm(w);
+reconstr = pvector*w + avg;
 reconstr = reshape(reconstr,imgsize);
 imshow(reconstr,[]);
