108e2dec9caa1ce78ec1d6a33bca4c84d1bbf20b
Yingjie <1275963@gmail.com>
2015-12-19 15:21:41 +0800

    Allows me to save figures as image

--- pca/visualpca.m
+++ pca/visualpca.m
@@ -3,21 +3,23 @@
 % plot(test);
 
 for k = 1:40:400
-    pause
     [perc, avg,pvector,data] = opca(k);
-    figure()
+    f = figure();
     
     imgsize = [112,92]; 
     pick = data(:,2);
 
-    subplot(1,2,1);
+    subplot(1,3,1);
     ori = reshape(pick,imgsize);
     imshow(ori,[]);
 
-    subplot(1,2,2);
+    subplot(1,3,2);
     w = pvector'*(pick-avg);
     reconstr = pvector*w +avg;
     reconstr = reshape(reconstr,imgsize);
     imshow(reconstr,[]);
     title(perc)
+    subplot(1,3,3)
+    plot(w)
+    saveas(f,[num2str(k),'.png']);
 end
