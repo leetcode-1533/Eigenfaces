72f867b8b2d5e813ec0db53100acfaf8af7545ce
Yingjie <1275963@gmail.com>
2015-12-21 16:16:56 +0800

    Working on recovery

--- nmf/nmftest.m
+++ nmf/nmftest.m
@@ -1,4 +1,9 @@
 imgsize = [112,92];
-data = imagedata();
-[w,h] = boardnmf(data  ,300,1);
-peekbase(w,imgsize);
+% data = imagedata();
+% [w,h] = boardnmf(data,300,1);
+% peekbase(w,imgsize);
+
+sapimg = w*h(:,1);
+sapimg = reshape(sapimg,imgsize);
+imshow(sapimg,[]);
+
