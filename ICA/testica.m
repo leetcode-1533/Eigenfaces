2692c276fcc3ca4d1e07e4513f7c7650dbf863fd
Yingjie <1275963@gmail.com>
2016-02-25 20:52:52 +0800

    Clean out

--- ICA/testica.m
+++ ICA/testica.m
@@ -4,7 +4,7 @@ imgsize = [112,92];
 
 images = imagedata(5,5);
 
-Z=fastica(images', 'numOfIC', 30);  
+Z=fastica(images', 'numOfIC', 30, 'displayMode', 'off', 'verbose', 'off');  
 peekbase(Z', imgsize, 10,3);
 
 
