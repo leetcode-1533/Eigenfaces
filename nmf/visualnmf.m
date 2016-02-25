5bf8df93c18cd8eee80a82df6dbe719c62c6434b
Yingjie <1275963@gmail.com>
2016-02-25 16:33:15 +0800

    Archive Progress

---
+++ nmf/visualnmf.m
@@ -0,0 +1,28 @@
+% Draw faces, recovered face, and their weight plot
+data = imagedata(40,3);
+pick = data(:,2);
+imgsize = [112,92]; 
+
+for k = 101:10:120
+    [w,h] = boardnmf(data,k,0);
+    for i = 1:k
+        w(:,i) = w(:,i)./norm(w(:,i));
+    end
+    f = figure();
+    
+    subplot(1,3,1);
+    ori = reshape(pick,imgsize);
+    imshow(ori,[]);
+
+    subplot(1,3,2);
+    wpinv = pinv(w);
+    weight = wpinv*pick;
+    reconstr = w*weight; 
+    reconstr = reshape(reconstr,imgsize);
+    imshow(reconstr,[]);
+    title(['Dimension',num2str(k)])
+    
+    subplot(1,3,3)
+    plot(weight)
+%     saveas(f,[num2str(k),'.png']);
+end
