044b8b450a7ca5ab757a83b8aad758e6a2e85b18
Yingjie <1275963@gmail.com>
2016-02-26 16:24:56 +0800

    Adopt PCA to use different size of data

--- pca/opca.m
+++ pca/opca.m
@@ -5,10 +5,11 @@ function [perc,avg,pvector,D] = opca(k, data)
 % pvector: the selected eigenvectors
 % data: the image data, 1 per column
 imgsize = [112,92]; 
+[~, y] = size(data);
 % data = imagedata();
 
 avg = mean(data,2);
-A = data - avg*ones(1, 400); % Centered at zero
+A = data - avg*ones(1, y); % Centered at zero
 L = A'*A;
 [V,D] = eig(L);
 % V: the decomposed eigenvectors, one per column associated with
