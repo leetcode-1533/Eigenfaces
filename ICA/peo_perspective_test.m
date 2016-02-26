7ce5c55978803b49d558b9877b2b7b6f59fed42c
Yingjie <1275963@gmail.com>
2016-02-26 15:29:49 +0800

    ICA part finished

--- ICA/peo_perspective_test.m
+++ ICA/peo_perspective_test.m
@@ -7,10 +7,31 @@ data = imagedata(40,3);
 sbase = fastica(data', 'numOfIC', 80, 'displayMode', 'off', 'verbose', 'off'); 
 inv_sbase = pinv(sbase);
 
-% different people
+%% different people
 
 % show original
 testdata = imagedata(10, 1);
+peekbase(testdata, imgsize, 5, 2);
+
+% reconstruct
+w = testdata' * inv_sbase;
+
+% show weights
+figure()
+hold on 
+legend()
+for i = 1: 10
+    plot(w(i, :));
+end
+
+% show reconstruct
+reconstr = w * sbase;
+peekbase(reconstr', imgsize, 5, 2);
+
+%% Different perspective
+
+% show original
+testdata = imagedata(1, 10);
 peekbase(testdata, imgsize, 5, 2);
 
 % reconstruct
