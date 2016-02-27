630dabbfbc45f5d076fcab526889d41251a1165d
Yingjie <1275963@gmail.com>
2016-02-27 20:20:02 +0800

    SVM wrong case now both original and DCT

--- compare/script_svm_wrongcases.m
+++ compare/script_svm_wrongcases.m
@@ -1,5 +1,20 @@
-inputcase = dctpredicted_labelp;
+%% Choose between Original and DCT
+% original
+inputcase = predicted_labeln;
+windex = find( inputcase - test_label ~= 0 );
+test_label(windex)
+svmoutput  = inputcase(windex)
+wronginput = test_people(:, windex);
+svmwrongout = imagedata2(svmoutput,1); 
+comp = [];
+for i = 1:length(windex)
+    comp = [comp,wronginput(:, i ), svmwrongout(:,i)];  
+end
+% peekbase(wronginput, imgsize, 9, 1)
+peekbase(comp, imgsize, length(windex), 2)
 
+% dct
+inputcase = dctpredicted_labelp;
 windex = find( inputcase - test_label ~= 0 );
 
 dct_test_people(windex)
@@ -12,5 +27,4 @@ comp = [];
 for i = 1:length(windex)
     comp = [comp,wronginput(:, i ), svmwrongout(:,i)];  
 end
-% peekbase(wronginput, imgsize, 9, 1)
 dct_peekbase(comp, imgsize, length(windex), 2)
