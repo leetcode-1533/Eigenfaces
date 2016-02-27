80f8f2ee02601d6ed9a5ca9cfa4da260aa56b50b
Yingjie <1275963@gmail.com>
2016-02-27 14:45:02 +0800

    SVM Fixed
    Previously, missing mapminmax

--- compare/PNI_Base.m
+++ compare/PNI_Base.m
@@ -7,9 +7,8 @@ function [pvector, pavg, wpinv, inv_sbase] = PNI_Base(nmfdata, k)
 for i = 1 : k
     w(:,i) = w(:,i)./norm(w(:,i));
 end
-
 wpinv = pinv(w); % nmf craeted weight
 
 sbase = fastica(nmfdata', 'numOfIC', k, 'displayMode', 'off', 'verbose', 'off');
-inv_sbase = pinv(sbase); % ica careted weight
+inv_sbase = pinv(sbase); % ica created weight
 
--- compare/svmcomp.m
+++ compare/svmcomp.m
@@ -1,43 +1,64 @@
-% clear 
-t = 5;
-n = 8;
-numofpeople = 40;
-numofperspective = 10;
-imgsize = [112,92]; 
+function svmpred = svmcomp() 
+%     t = 5;
+    n = 8;
+    numofpeople = 40;
+    numofperspective = 10;
+    imgsize = [112,92]; 
 
-% read in data
+    % read in data
 
-train_people = zeros(imgsize(1) * imgsize(2), n * numofpeople);
-train_label = zeros(n * numofpeople , 1);
-test_people = [];
-test_label = [];
+    train_people = zeros(imgsize(1) * imgsize(2), n * numofpeople);
+    train_label = zeros(n * numofpeople , 1);
+    test_people = [];
+    test_label = [];
 
-for i = 1 : numofpeople
-    trs = randperm(numofperspective);
-    train_people(:, ((i - 1) * n + 1) : i * n)  = imagedata2(i, trs(1 : n));
-    train_label(((i - 1) * n + 1) : i * n,  1) =  repmat(i, n, 1);
-    
-    test_people = [test_people, imagedata2(i, trs(n+1 : numofperspective))];
-    test_label = [test_label; repmat(i, numofperspective - n, 1)];
-end
+    for i = 1 : numofpeople
+        trs = randperm(numofperspective);
+        train_people(:, ((i - 1) * n + 1) : i * n)  = imagedata2(i, trs(1 : n));
+        train_label(((i - 1) * n + 1) : i * n,  1) =  repmat(i, n, 1);
 
-% shuffle
-perm1 = randperm(size(train_people,2));
-train_people = train_people(:, perm1);
-train_label = train_label(perm1);
+        test_people = [test_people, imagedata2(i, trs(n+1 : numofperspective))];
+        test_label = [test_label; repmat(i, numofperspective - n, 1)];
+    end
 
-perm2 = randperm(size(test_people,2));
-test_people = test_people(:, perm2);
-test_label = test_label(perm2);
+    % shuffle
+    perm1 = randperm(size(train_people,2));
+    train_people = train_people(:, perm1);
+    train_label = train_label(perm1);
 
-% NMF, ICA, PCA Base
+    perm2 = randperm(size(test_people,2));
+    test_people = test_people(:, perm2);
+    test_label = test_label(perm2);
 
-nmfdata = imagedata2(1:20,1:6); % triaing sets: adjustable
-% [pvector, pavg, wpinv, inv_sbase] = PNI_Base(nmfdata, 10);
+    % NMF, ICA, PCA Base
+
+    nmfdata = imagedata2(1:20,1:6); % triaing sets: adjustable
+    [pvector, pavg, wpinv, inv_sbase] = PNI_Base(nmfdata, 30);
 
 
-[train_p, train_n, train_i] = PNI_Projection(pvector, pavg, wpinv, inv_sbase, train_people);
-[test_p, test_n, test_i] = PNI_Projection(pvector, pavg, wpinv, inv_sbase, test_people);
+    [train_p, train_n, train_i] = PNI_Projection(pvector, pavg, wpinv, inv_sbase, train_people);
+    [test_p, test_n, test_i] = PNI_Projection(pvector, pavg, wpinv, inv_sbase, test_people);
 
-model = svmtrain(train_label, train_i');
-[predicted_label, accur, ~] = svmpredict(test_label, test_i', model);
+    train = train_i;
+    test = test_i;
+    [re_train, ps] = mapminmax(train); 
+    re_test = mapminmax.apply(test,ps);
+    model = svmtrain(train_label, re_train');
+    [predicted_label, accurp, ~] = svmpredict(test_label, re_test', model);
+
+    train = train_i;
+    test = test_i;
+    [re_train, ps] = mapminmax(train); 
+    re_test = mapminmax.apply(test,ps);
+    model = svmtrain(train_label, re_train');
+    [predicted_label, accurn, ~] = svmpredict(test_label, re_test', model);
+
+    train = train_i;
+    test = test_i;
+    [re_train, ps] = mapminmax(train); 
+    re_test = mapminmax.apply(test,ps);
+    model = svmtrain(train_label, re_train');
+    [predicted_label, accuri, ~] = svmpredict(test_label, re_test', model);
+    svmpred = [accurp(1), accurn(1), accuri(1)];
+
+
