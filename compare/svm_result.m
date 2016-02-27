9baf53ba64725733609b2d9c6e51b5ee9cf1a7e2
Yingjie <1275963@gmail.com>
2016-02-27 16:05:13 +0800

    Now the same input to both dct and original

--- compare/svm_result.m
+++ compare/svm_result.m
@@ -4,6 +4,10 @@ numofperspective = 10;
 imgsize = [112,92];
 
 % read in data
+train_people = zeros(imgsize(1) * imgsize(2), n * numofpeople);
+train_label = zeros(n * numofpeople , 1);
+test_people = [];
+test_label = [];
 
 dct_train_people = zeros(imgsize(1) * imgsize(2), n * numofpeople);
 dct_train_label = zeros(n * numofpeople , 1);
@@ -12,20 +16,33 @@ dct_test_label = [];
 
 for i = 1 : numofpeople
     trs = randperm(numofperspective);
-    dct_train_people(:, ((i - 1) * n + 1) : i * n)  = dct_imagedata(i, trs(1 : n));
-    dct_train_label(((i - 1) * n + 1) : i * n,  1) =  repmat(i, n, 1);
+ 
+    train_people(:, ((i - 1) * n + 1) : i * n)  = imagedata2(i, trs(1 : n));
+    train_label(((i - 1) * n + 1) : i * n,  1) =  repmat(i, n, 1);
+    test_people = [test_people, imagedata2(i, trs(n+1 : numofperspective))];
+    test_label = [test_label; repmat(i, numofperspective - n, 1)];
     
+    dct_train_people(:, ((i - 1) * n + 1) : i * n)  = dct_imagedata(i, trs(1 : n));
+    dct_train_label(((i - 1) * n + 1) : i * n,  1) =  repmat(i, n, 1);   
     dct_test_people = [dct_test_people, dct_imagedata(i, trs(n+1 : numofperspective))];
     dct_test_label = [dct_test_label; repmat(i, numofperspective - n, 1)];
+
 end
 
 % shuffle
 perm1 = randperm(size(dct_train_people,2));
+train_people = train_people(:, perm1);
+train_label = train_label(perm1);
 dct_train_people = dct_train_people(:, perm1);
 dct_train_label = dct_train_label(perm1);
 
 perm2 = randperm(size(dct_test_people,2));
+test_people = test_people(:, perm2);
+test_label = test_label(perm2);
 dct_test_people = dct_test_people(:, perm2);
 dct_test_label = dct_test_label(perm2);
     
-dct_svmcomp(dct_train_people, dct_train_label, dct_test_people, dct_test_label, 30)
+dct = dct_svmcomp(dct_train_people, dct_train_label, dct_test_people, dct_test_label, 30);
+ori = svmcomp(train_people, train_label, test_people, test_label, 30);
+ans = [ori, dct]
+
