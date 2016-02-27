3d566e75657e2ca35f87c36597eb8e12fe0582d3
Yingjie <1275963@gmail.com>
2016-02-27 19:04:00 +0800

    Dct show wrong cases

--- compare/script_svm_wrongcases.m
+++ compare/script_svm_wrongcases.m
@@ -1,16 +1,16 @@
-inputcase = predicted_labeln;
+inputcase = dctpredicted_labelp;
 
 windex = find( inputcase - test_label ~= 0 );
 
-test_label(windex)
+dct_test_people(windex)
 svmoutput  = inputcase(windex)
 
-wronginput = test_people(:, windex);
-svmwrongout = imagedata2(svmoutput,1); 
+wronginput = dct_test_people(:, windex);
+svmwrongout = dct_imagedata(svmoutput,1); 
 
 comp = [];
 for i = 1:length(windex)
     comp = [comp,wronginput(:, i ), svmwrongout(:,i)];  
 end
 % peekbase(wronginput, imgsize, 9, 1)
-peekbase(comp, imgsize, length(windex), 2)
+dct_peekbase(comp, imgsize, length(windex), 2)
