144758e70218a0827b387a78ceeb8f254a89e6ec
Yingjie <1275963@gmail.com>
2016-03-07 21:18:30 +0800

    KD search

--- kdtree/nearestperson.m
+++ kdtree/nearestperson.m
@@ -4,12 +4,37 @@ imgsize = [112,92];
 pcadata = imagedata2(1:40, 1:10);
 
 [~, pavg, pvector, ~] = opca(30, pcadata);
-[pw, ~, ~] = PNI_Projection(pvector, pavg, 0, 0, pcadata);
 
+
+
+file = dir('/Users/y1275963/Desktop/imgs');
+NF = length(file);
+imgdata = [];
+
+retable = zeros(NF, 10); % Assume have 10 people, which is impossible
+% images = cell(NF,1);
+for k = 3 : NF  
+    me = imread(fullfile('/Users/y1275963/Desktop/imgs', file(k).name));
+    FDetect = vision.CascadeObjectDetector;
+    BB = step(FDetect, me);
+    for item = 1 : size(BB, 1) 
+        i2 = imcrop(me, BB(item, :));
+        i2 = rgb2gray(i2);
+        i2 = imresize(i2, imgsize);
+        i2 = double(i2);
+        i2w = PNI_Projection(pvector, pavg, 0, 0, i2(:));
+        imgdata = [imgdata, i2w]; 
+        retable(k - 2, item) = size(imgdata, 2);
+    end
+%     imshow(imread(fullfile('/Users/y1275963/Desktop/imgs', file(k).name)));
+%   images{k} = imread(fullfile('image.orig', file(k).name));
+end
+
+
+[pw, ~, ~] = PNI_Projection(pvector, pavg, 0, 0, pcadata);
 mdl = KDTreeSearcher(pw');
 
-
-me = imread('/Users/y1275963/Downloads/lfw/Ann_Godbehere/Ann_Godbehere_0001.jpg');
+me = imread('/Users/y1275963/Pictures/me.png');
 FDetect = vision.CascadeObjectDetector;
 BB = step(FDetect, me);
 i2 = imcrop(me, BB);
@@ -17,8 +42,23 @@ i2 = rgb2gray(i2);
 i2 = imresize(i2, imgsize);
 i2 = double(i2);
 i2w = PNI_Projection(pvector, pavg, 0, 0, i2(:));
+% idx = knnsearch(mdl, i2w');
 
-idx = knnsearch(mdl, i2w');
-
-temp = [i2(:), pcadata(:, idx)];
-peekbase(temp, imgsize, 1, 2)
+res = rangesearch(mdl, i2w', 3300);
+for idx = res{1}
+    [x, ~] = find(retable == idx);
+    k = 2 + x;
+    similar = imread(fullfile('/Users/y1275963/Desktop/imgs', file(k).name));
+    
+    pause
+    subplot(1, 2, 1)
+    imshow(me, []);
+    subplot(1, 2, 2)
+    imshow(similar, []);
+end
+% if ~ifempty(k)
+%     subplot(1, 2, 1)
+%     imshow(me, []);
+%     subplot(1, 2, 2)
+%     imshow(similar, []);
+% end
