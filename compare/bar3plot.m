bb9e7a401873869d30815da7e7b26b0d7b9b6ee8
Yingjie <1275963@gmail.com>
2016-03-19 18:13:13 +0800

    slightly modified bar3plot range for this experiment

--- compare/bar3plot.m
+++ compare/bar3plot.m
@@ -1,8 +1,10 @@
 function bar3plot(vari)
+% modify the x, y range when using
     bar3(vari);
 
     tick = get(gca,{'XTick','YTick'});
-    x = linspace(5, 100,length(tick{1}));
+%     x = linspace(5, 100,length(tick{1}));
+    x = 2 : 5 :50;
     y = linspace(1, 9,length(tick{2}));
     set(gca,'XTickLabel',x)
     set(gca,'YTickLabel',y)
