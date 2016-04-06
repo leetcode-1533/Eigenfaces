46192d70df756d962a03a5313b4764d499f870c9
Yingjie <1275963@gmail.com>
2016-04-06 16:02:21 +0800

    PNI distance matrix

---
+++ distance_matrix/PNI_dis_mat.m
@@ -0,0 +1,67 @@
+% basedata = imagedata2(1:40,1:3);  
+% testdata = imagedata2(1:40, 1:10);
+% 
+% [pvector, pavg, wpinv, inv_sbase] = PNI_Base(basedata, 64);
+% [train_p, train_n, train_i] = PNI_Projection(pvector, pavg, wpinv, inv_sbase, testdata);
+
+pca_matrix = pdist2(train_p', train_p', 'euclidean');
+nmf_matrix = pdist2(train_n', train_n', 'euclidean');
+ica_matrix = pdist2(train_i', train_i', 'euclidean');
+
+title('Projection Distance')
+subplot(2,3,1)
+colormap('hot');   % set colormap
+imagesc(pca_matrix);        % draw image and scale colormap to values range
+col = colorbar;          % show color scale
+col.Ticks = [];
+grid on
+axis off
+title('PCA Eulidean Distance')
+
+subplot(2,3,2)
+colormap('hot');   % set colormap
+imagesc(nmf_matrix);        % draw image and scale colormap to values range
+col = colorbar;          % show color scale
+col.Ticks = [];
+grid on
+axis off
+title('NMF Eulidean Distance')
+
+subplot(2,3,3)
+colormap('hot');   % set colormap
+imagesc(ica_matrix);        % draw image and scale colormap to values range
+col = colorbar;          % show color scale
+col.Ticks = [];
+grid on
+axis off
+title('ICA Eulidean Distance')
+
+pca_matrix = pdist2(train_p', train_p', 'cosine');
+nmf_matrix = pdist2(train_n', train_n', 'cosine');
+ica_matrix = pdist2(train_i', train_i', 'cosine');
+
+subplot(2,3,4)
+colormap('hot');   % set colormap
+imagesc(pca_matrix);        % draw image and scale colormap to values range
+col = colorbar;          % show color scale
+grid on
+axis off
+title('PCA Cosine Distance')
+
+subplot(2,3,5)
+colormap('hot');   % set colormap
+imagesc(nmf_matrix);        % draw image and scale colormap to values range
+col = colorbar;          % show color scale
+grid on
+axis off
+title('NMF Cosine Distance')
+
+subplot(2,3,6)
+colormap('hot');   % set colormap
+imagesc(ica_matrix);        % draw image and scale colormap to values range
+col = colorbar;          % show color scale
+grid on
+axis off
+title('ICA Cosine Distance')
+
+
