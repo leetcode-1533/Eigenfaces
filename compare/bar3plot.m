bbbfa9d9b57bd3aa2f5cc3b5e37af2870ffab186
Yingjie <1275963@gmail.com>
2016-04-08 19:15:57 +0800

    svm plot

--- compare/bar3plot.m
+++ compare/bar3plot.m
@@ -1,11 +1,10 @@
-function bar3plot(vari)
+function bar3plot(x, y, vari)
 % modify the x, y range when using
     bar3(vari);
 
     tick = get(gca,{'XTick','YTick'});
 %     x = linspace(5, 100,length(tick{1}));
-    x = 2 : 5 :50;
-    y = linspace(1, 9,length(tick{2}));
+%     x = 2 : 5 :50;
+%     y = linspace(1, 9,length(tick{2}));
     set(gca,'XTickLabel',x)
     set(gca,'YTickLabel',y)
-    view(230, 33)
--- compare/svm_gridscanner.m
+++ compare/svm_gridscanner.m
@@ -3,18 +3,77 @@ basesize = 5:5:100;
 % basesize = 10:5:15;
 
 [xloc, yloc] = meshgrid(intern, basesize);
-PCA = [];
-NMF = [];
-ICA = [];
-DCTPCA = [];
-DCTICA = [];
-for iterbase = basesize
-    pni_od = svm_original_dct_scanner(intern, iterbase);
-    PCA = [PCA, pni_od(:, 1)];
-    NMF = [NMF, pni_od(:, 2)];
-    ICA = [ICA, pni_od(:, 3)];
-    DCTPCA = [DCTPCA, pni_od(:, 4)];
-    DCTICA = [DCTICA, pni_od(:, 5)];
-end
-    
-    
+% PCA = [];
+% NMF = [];
+% ICA = [];
+% DCTPCA = [];
+% DCTICA = [];
+% for iterbase = basesize
+%     pni_od = svm_original_dct_scanner(intern, iterbase);
+%     PCA = [PCA, pni_od(:, 1)];
+%     NMF = [NMF, pni_od(:, 2)];
+%     ICA = [ICA, pni_od(:, 3)];
+%     DCTPCA = [DCTPCA, pni_od(:, 4)];
+%     DCTICA = [DCTICA, pni_od(:, 5)];
+% end
+
+%%For Combination Plot
+hold on
+ob1 = mesh(xloc, yloc, PCA','faceColor','r');
+ob2 = mesh(xloc, yloc, NMF','faceColor','b'); 
+ob3 = mesh(xloc, yloc, ICA','faceColor','g');
+hlegend = legend([ob1, ob2, ob3], {'PCA','NMF','ICA'});
+set(hlegend,'FontSize',23);
+
+title('SVM Recognition Test')
+xlabel('Learning Sample Rate')
+ylabel('Vector Length')
+zlabel('Recogniton Rate')
+
+
+%% For seperate plot
+% figure(1)
+%     subplot(1,3,1);
+%      bar3plot(1:3:10,basesize, PCA)
+%     title('PCA');
+%     view(230, 33)
+%     
+%     subplot(1,3,2);
+%      bar3plot(1:3:10, basesize, NMF)
+%     title('NMF');
+%     view(230, 33)
+%     
+%     subplot(1,3,3);
+%      bar3plot(1:3:10, basesize, ICA)
+%     title('ICA');
+%     view(230, 33)
+%     
+% sdf(1, 'tk');
+% export_fig('/Users/y1275963/Dropbox/thesis/Img/fig/svm_pni','-pdf')
+
+%% Filter
+% figure(1)
+%     subplot(1,3,1);
+%     PCA70 = PCA;
+%     PCA70(PCA70<70) = 0;
+%      bar3plot(1:3:10, 5:30:100, PCA70)
+%     title('PCA > 70');
+%     view(230, 33)
+%     
+%     subplot(1,3,2);
+%     PCA80 = PCA;
+%     PCA80(PCA80<80) = 0;
+%      bar3plot(1:3:10, 5:30:100, PCA80)
+%     title('PCA > 80');
+%     view(230, 33)
+%     
+%     subplot(1,3,3);
+%     PCA85 = PCA;
+%     PCA85(PCA85<85) = 0;
+%      bar3plot(1:3:10, 5:30:100, PCA80)
+%     title('PCA > 85');
+%     view(230, 33)
+%     
+% sdf(1, 'tk');
+% export_fig('/Users/y1275963/Dropbox/thesis/Img/fig/svm_pcafilter','-png', '-m2.5')
+
